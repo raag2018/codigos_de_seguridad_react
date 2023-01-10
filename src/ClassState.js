ba5b76b52c43f87d9b13407f5d3f8028ebef7b0b
Roberto Antonio Alferes Gomez <roberto3652017@gmail.com>
2023-01-09 20:57:37 -0600

    actualizacion de estados

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { Loading } from "./Loading";
 
 class ClassState extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       error: false,
+      loading: true,
     };
+  }
+  /*UNSAFE_componentWillMount(){
+    console.log("componentWillMount");
+  }
+
+  componentDidMount(){
+    console.log("componentDidMount");
+  }*/
+  componentDidUpdate(){
+    console.log("update");
+    if(!!this.state.loading){
+      setTimeout(() => {
+        console.group("Validacion");
+        console.log("haciendo la validacion");
+        this.setState({loading: false});
+        console.log("restableciendo loading a false");
+        console.log("terminado la validacion");
+        console.groupEnd();
+      },3000);
+    }
   }
   render() {
     let name = this.props.name;
@@ -16,9 +38,12 @@ class ClassState extends React.Component {
           {
             this.state.error && (<p>Error: el codigo es incorrecto.</p>)
           }
+          {
+            this.state.loading && (<Loading/>)
+          }
           <input placeholder="Codigo de Seguridad"/>
           <button
-            onClick={() => this.setState(prevState => ({error: !prevState.error}))}
+            onClick={() => this.setState({loading: true})}
           >Comprobar</button>
         </div>
     );
--- src/UseState.js
+++ src/UseState.js
@@ -1,17 +1,53 @@
 import React from "react";
-
+const SECURITY_CODE = "paradigma";
 function UseState({name}) {
+  const [value, setValue] = React.useState("");
   const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  console.log(value);
+  React.useEffect(() => {
+    console.group("useEffect");
+    console.log("Empezando el efecto");
+    if(!!loading){
+      setTimeout(() => {
+        console.group("Validacion");
+        console.log("haciendo la validacion");
+        console.log(error);
+        if(value !== SECURITY_CODE){
+          setError(true);
+        }else{
+          setError(false);
+        }
+        setLoading(false);
+        console.log("restableciendo loading a false");
+        console.log("terminado la validacion");
+        console.groupEnd();
+      },1500);
+    }
+    console.log("Terminando el efecto");
+    console.groupEnd();
+  },[loading]);
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>Por favor, escribe el codigo de seguridad.</p>
-      {error && (
+      {(error && !loading) && (
         <p>Error: el codigo es incorrecto</p>
       )}
-      <input placeholder="Codigo de Seguridad" />
+      {loading && (
+        <p>Cargando...</p>
+      )}
+      <input 
+      value={value}
+      onChange={(event) => {
+        //setError(false);
+        setValue(event.target.value);
+      }}
+      placeholder="Codigo de Seguridad" />
       <button
-        onClick={() => setError(!error)}
+        onClick={() => {
+          setError(false);
+          setLoading(true);}}
       >Comprobar</button>
     </div>
   );
